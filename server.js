e807e596eec0b4526699642e95badfb27eb65e5f
Rugiyya Mammadov <rugiyamammadova@gmail.com>
2020-03-23 15:56:21 -0400

    created a post messages service

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 var express = require('express');
-
+var bodyParser = require("body-parser")
 var app = express()
 
 app.use(express.static(__dirname))
+app.use(bodyParser.json())
+var messages = [
+  { name: "Rugi", message: "Hi" },
+  { name: "Ali", message: "Hello" },
+]
+
+app.get("/messages", (req, res) => {
+  res.send(messages)
+})
+
+app.post("/messages", (req, res) => {
+  messages.push(req.body)
+  res.sendStatus(200)
+})
 
 var server = app.listen(3000, () => {
   console.log('server is lising on port', server.address().port)
