16013a66b00f9fdb85a6436a5ca6673d39edf914
Rugiyya Mammadov <rugiyamammadova@gmail.com>
2020-03-22 20:06:51 -0400

    created a browser app

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+
+var app = express()
+
+app.use(express.static(__dirname))
+
+var server = app.listen(3000, () => {
+  console.log('server is lising on port', server.address().port)
+})
+
